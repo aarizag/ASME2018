48515380d60ec157bf5a43378f4a8bca3daf8b67
aarizag <adr.arizaga@gmail.com>
2018-02-02 18:13:29 -0800

    Non-Streamline, single joystick control of servos

--- ps3_control_servos/ps3_control_servos.ino
+++ ps3_control_servos/ps3_control_servos.ino
@@ -6,12 +6,12 @@ USB Usb;
 BTD Btd(&Usb);
 PS3BT PS3(&Btd);
 
-Servo servo1;                                                         //Create instances of type Servo. servo1 is the steering servo and servo2 is the ESC.
-Servo servo2;
+Servo right;                                                         //Create instances of type Servo. servo1 is the steering servo and servo2 is the ESC.
+Servo left;
 bool start = false;
 
 void setup() {
-   Serial.begin(115200);
+   Serial.begin(9600);
   
    if (Usb.Init() == -1) {
     Serial.print(F("\r\nOSC did not start"));
@@ -19,6 +19,9 @@ void setup() {
   }
   Serial.print(F("\r\nPS3 Bluetooth Library Started"));
 }
+
+int x;
+int y;
 void loop()
 {
  
@@ -29,39 +32,46 @@ void loop()
 
   if(PS3.getButtonPress(PS)) {
     start = true;
-    if (!servo1.attached()){
-      servo1.attach(3);                                               
-      servo2.attach(5);
+    if (!right.attached()){
+      right.attach(3);                                               
+      left.attach(5);
     }
   }
   
   if (start) {
-    servo1.write(map(PS3.getAnalogHat(RightHatX), 0, 1023, 0, 180));
-    servo2.write(map(PS3.getAnalogHat(LeftHatY), 0, 1023, 180, 0));
+    x = PS3.getAnalogHat(LeftHatX);
+    Serial.print("X: ");
+    Serial.println(x);
+    y = PS3.getAnalogHat(LeftHatY);
+    Serial.print("Y: ");
+    Serial.print(y);
+
+  // Wheels move forward at 0
+  // Wheels stand still at ~122
+  // x Axis 0 is left
+  // y axis 0 is top
+  // ROTATE LEFT : xy=(0,122) -> rl=(0, 255) 
+  // ROTATE RIGHT: xy=(255, 122) -> rl=(255, 0)
+      // y = 255 - x
+      
+  // FORWARD: xy=(122,0) -> rl=(0, 0) 
+      // x = y
+  // BACKWARD: xy=(122,255) -> rl=(255, 255)
+      // x = y
+      
+
+    right.write(map(x, 0, 1023, 0, 180));
+    left.write(map(y, 0, 1023, 180, 0)); //CHANGED
 //    Serial.println(map(PS3.getAnalogHat(RightHatX), 0, 1023, 0, 180));
   }
-//  if (PS3.getButtonClick(UP)) {
-//    digitalWrite(LED1, HIGH);
-//
-//    Serial.print(F("\r\nUp"));
-//
-//     if (PS3.PS3Connected) {
-//      PS3.setLedOff();
-//      PS3.setLedOn(LED1);
-//
-//    }
-//  }
-//    
-//  if (PS3.getButtonClick(UP)) {
-//    digitalWrite(LED1, HIGH);
-//
-//    Serial.print(F("\r\nUp"));
-//
-//     if (PS3.PS3Connected) {
-//      PS3.setLedOff();
-//      PS3.setLedOn(LED1);
-//
-//    }
-//  }
 }
 
+int rightWheelMove(x, y){
+  int value = 0;
+  if (value < 50){
+    return 0;
+  }
+  else{
+    return value;
+  }
+}

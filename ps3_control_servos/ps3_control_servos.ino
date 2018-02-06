4feb74cdc7bb6ec1b22f9188629b20728d7c1a9c
aarizag <adr.arizaga@gmail.com>
2018-02-05 16:28:39 -0800

    Finished Streamlined PS3 analog controls.

--- ps3_control_servos/ps3_control_servos.ino
+++ ps3_control_servos/ps3_control_servos.ino
@@ -1,12 +1,15 @@
+/*Include the necessary libraries.*/
 #include <SPI.h>
-#include <PS3BT.h>                                                    //Include the necessary libraries.
+#include <PS3BT.h>
 #include <Servo.h>
 
+/* Create global Variables for Startup*/
 USB Usb;
 BTD Btd(&Usb);
 PS3BT PS3(&Btd);
 
-Servo right;                                                         //Create instances of type Servo. servo1 is the steering servo and servo2 is the ESC.
+/*Create instances of type Servo. 'right' is the right side servo and 'left' is the left side servo.*/
+Servo right;                                                         
 Servo left;
 bool start = false;
 
@@ -20,19 +23,21 @@ void setup() {
   Serial.print(F("\r\nPS3 Bluetooth Library Started"));
 }
 
-int x;
-int y;
+/* Create global Variables for loop*/
+int x;         // Raw x-axis input from analog
+int y;         // Raw y-axis input from analog
+int leftVal;   // Altered value to be used in the left servo 
+int rightVal;  // Altered value to be used in the right servo
 void loop()
 {
  
   Usb.Task();
   
-//  Serial.println("PS3 Connected");
-//  Serial.println(PS3.PS3Connected);
-
   if(PS3.getButtonPress(PS)) {
+    // Do not start the program until the PS3 PS button has been pressed
     start = true;
     if (!right.attached()){
+      // Attach servos to power once PS button is pressed
       right.attach(3);                                               
       left.attach(5);
     }
@@ -40,38 +45,40 @@ void loop()
   
   if (start) {
     x = PS3.getAnalogHat(LeftHatX);
-    Serial.print("X: ");
-    Serial.println(x);
+//    Serial.print("X: ");
+//    Serial.println(x);
     y = PS3.getAnalogHat(LeftHatY);
-    Serial.print("Y: ");
-    Serial.print(y);
+//    Serial.print("Y: ");
+//    Serial.print(y);
 
-  // Wheels move forward at 0
-  // Wheels stand still at ~122
-  // x Axis 0 is left
-  // y axis 0 is top
-  // ROTATE LEFT : xy=(0,122) -> rl=(0, 255) 
-  // ROTATE RIGHT: xy=(255, 122) -> rl=(255, 0)
-      // y = 255 - x
-      
-  // FORWARD: xy=(122,0) -> rl=(0, 0) 
-      // x = y
-  // BACKWARD: xy=(122,255) -> rl=(255, 255)
-      // x = y
-      
-
-    right.write(map(x, 0, 1023, 0, 180));
-    left.write(map(y, 0, 1023, 180, 0)); //CHANGED
-//    Serial.println(map(PS3.getAnalogHat(RightHatX), 0, 1023, 0, 180));
+    leftVal = y - (x - 127);
+    Serial.print("Left: ");
+    Serial.print(valCap(leftVal));
+    
+    rightVal = x + (y - 127);
+    Serial.print("Right: ");
+    Serial.println(valCap(rightVal));
+    
+    right.write(map(valCap(rightVal), 0, 1023, 0, 180));
+    left.write(map(valCap(leftVal), 0, 1023, 180, 0)); 
+    
   }
 }
 
-int rightWheelMove(x, y){
-  int value = 0;
-  if (value < 50){
-    return 0;
+  /* Cap maximum value at 255
+   * cap minimum value at 0
+   * create "deadzone" in the middle of the analog to prevent servo drift
+   */
+  
+int valCap(int val){
+  
+  if (val > 122 && val < 132){
+    return 127;
   }
-  else{
-    return value;
+  else if (val > 250){
+    return 255;
+  }
+  else if (val < 5) {
+    return 0;
   }
 }

2428ec554edffac4aee9812c6078c8cde71fb309
aarizag <adr.arizaga@gmail.com>
2018-03-13 15:13:05 -0700

    Version 2.0 of dc motor control sketch.

--- dc_motor_control/dc_motor_control.ino
+++ dc_motor_control/dc_motor_control.ino
@@ -1,47 +1,86 @@
 #include <SPI.h>
 #include <PS3BT.h>
-#include <DCMotor.h>
+#include <Servo.h>
 
- 
-/* Create global Variables for Startup*/
-USB Usb;
-BTD Btd(&Usb);
-PS3BT PS3(&Btd);
+USB usb;
+BTD btd(&usb);
+PS3BT ps3(&btd);
 
-/*TODO Get Motor Pin from EEs*/
-DCMotor right;
-DCMotor left;
- 
-void setup() 
-{ 
-  pinMode(motorPin, OUTPUT);
+const byte mtr1 [2] = {9,10}; // motor 1 pins
+//const byte mtr2 [2]; // motor 2 pins
+/* index 0 = pwm
+ * index 1 = dir */
+
+void setup(){
   Serial.begin(9600);
-  if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
-    while (1); //halt
+
+  pinMode(mtr1[0],OUTPUT); //declare motor1 pwm pin as OUTPUT
+  pinMode(mtr1[1],OUTPUT); //declare motor2 dir pin as OUTPUT
+
+    if (usb.Init() == -1) {
+    Serial.print("USB failed to start");
+    while(1); // STOP
   }
-  Serial.print(F("\r\nPS3 Bluetooth Library Started"));
-} 
+  Serial.println("PS3 Bluetooth Started");
+}
+
+int xIN; // x input from the ps3 controller
+int yIN; // y input
+int xOUT; // modified x output to wheels
+int yOUT; // modified y
 
 
-/* Create global Variables for loop*/
-int leftVal;   // Altered value to be used in the left servo 
-int rightVal;  // Altered value to be used in the right servo
-int x;         // Raw x-axis input from analog
-int y;         // Raw y-axis input from analog
-           // x and y will return a value between 0 and 255
-           // 0 will rotate servo forward, 255 will rotate backwards
-           
-bool start = false;  // Bool variable used to prevent premature servo rotation
+bool start = false;
 
-void loop() 
-{ 
-  if (Serial.available())
-  {
-    int speed = Serial.parseInt();
-    if (speed >= 0 && speed <= 255)
-    {
-      analogWrite(motorPin, speed);
+void loop (){
+    usb.Task();
+
+    if(ps3.getButtonPress(PS)) {
+        // Do not start the program until the PS3 PS button has been pressed
+        start = true;
     }
-  }
-} 
+
+    if (start) {
+        xIN = ps3.getAnalogHat(LeftHatX);
+        yIN = ps3.getAnalogHat(LeftHatY);
+
+        yOUT = valCap(yIN - (xIN - 127));
+        xOUT = valCap(xIN + (yIN - 127));
+
+//        String debug = "x: " + xIN + " -> " + xOUT + " | " + "y: " + yIN + " -> " + yOUT;
+//        Serial.println(debug);
+
+        if (xOUT < 127){
+            digitalWrite(mtr1[1],HIGH);
+            Serial.println(valCap(((255-xOUT) -127) * 2));
+            analogWrite(mtr1[0],valCap(((255-xOUT) -127) * 2));
+        }
+        else {
+            digitalWrite(mtr1[1],LOW);
+            Serial.println(valCap((xOUT-127) * 2));
+            analogWrite(mtr1[0],valCap((xOUT-127) * 2));
+        }
+        if (yOUT < 127){
+            digitalWrite(mtr1[1],HIGH);
+            Serial.println(valCap(((255-yOUT) -127) * 2));
+            analogWrite(mtr1[0],valCap(((255-yOUT) -127) * 2));
+        }
+        else {
+            digitalWrite(mtr1[1],LOW);
+            Serial.println(valCap((yOUT-127) * 2));
+            analogWrite(mtr1[0],valCap((yOUT-127) * 2));
+        }
+    }
+}
+
+int valCap(int val){
+    if (val > 122 && val < 132){
+        return 127;
+    }
+    else if (val > 250){
+        return 255;
+    }
+    else if (val < 5) {
+        return 0;
+    }
+}
